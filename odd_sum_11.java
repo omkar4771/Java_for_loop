ff6e3c0b1ae19d4d63a783c4c619e3443a671769
omkar4771 <omkardaswadkar123@gmail.com>
2024-05-05 23:39:45 +0530

    odd number sum

---
+++ odd_sum_11.java
@@ -0,0 +1,20 @@
+package for_loop;
+import java.util.*;
+public class odd_sum_11 {
+
+	public static void main(String[] args)
+	{
+		int i ,n, sum=0;
+	    Scanner sc=new Scanner(System.in);
+		System.out.println("enter one Number");
+		n=sc.nextInt();
+		for(i=1;i<=n;i=i+2)
+		{
+		    sum=sum+i;
+		    
+		}
+		System.out.println("sum="+sum);
+		
+	}
+
+}
