d1ab6626a3a08ebadeda11eb3678cb62098b8b2a
omkar4771 <omkardaswadkar123@gmail.com>
2024-05-05 23:37:01 +0530

    Even number

---
+++ even_num_06.java
@@ -0,0 +1,17 @@
+package for_loop;
+import java.util.*;
+public class even_num_06 {
+
+	public static void main(String[] args) 
+	{
+		int i,n;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter 1 number");
+		n=sc.nextInt();
+	for(i=2;i<=n;i=i+2)
+	{
+	System.out.print("  "+i);
+	}
+	}
+
+}
